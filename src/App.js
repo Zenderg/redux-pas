5e74c2470ab641801cc6fae3059a1ec274cd0c93
dm.kuzin <danil54658798@gmail.com>
2019-07-25 11:54:33 +0300

    connect app with store

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 import './App.css'
 
-export default class App extends Component {
+class App extends Component {
+  static mapStateToProps = store => {
+    console.log(store) // посмотрим, что же у нас в store?
+    return {
+      user: store.user,
+    }
+  }
+
   render() {
     return (
       <div className="app">
-        <header className="app-header">
-          <h1 className="app-title">Мой топ фоток за 2007 год :</h1>
+        <header className="app__header">
+          <h1 className="app__title">Мой топ фоток за 2007 год</h1>
+          <p className="app__name">Пользователь: {this.props.user}</p>
         </header>
       </div>
     )
   }
 }
+
+export default connect(App.mapStateToProps)(App)
